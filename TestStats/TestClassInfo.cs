ba15d5b7ccf112b3774c5ec7995608c39d020b0c
RdPhnX <volodymyr.vedmedera@gmail.com>
2019-07-03 23:50:55 +0300

    Add project files.

---
+++ TestStats/TestClassInfo.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TestStats
+{
+    public class TestClassInfo
+    {
+        public TestClassInfo(TypeInfo typeInfo)
+        {
+            if (!typeInfo.IsClass)
+            {
+                return;
+            }
+            IEnumerable<CustomAttributeData> attrDataList = typeInfo.CustomAttributes;
+            IEnumerable<CustomAttributeData> ignoreAttrData = attrDataList.Where(val => val.AttributeType == typeof(IgnoreAttribute)).Distinct();
+            TestClassName = typeInfo.Name;
+            IgnoreValues = ignoreAttrData.Any()
+                        ? ignoreAttrData.ToList().FirstOrDefault().ConstructorArguments.Where(val => val.ArgumentType == typeof(String))
+                            .FirstOrDefault().Value.ToString().Split(',')
+                            .Select(val => val.Trim()).ToArray()
+                        : default;
+            IEnumerable<MethodInfo> testMethods = typeInfo.GetMethods()
+                .Where(method => method.CustomAttributes.Any(attr => attr.AttributeType == typeof(TestAttribute)));
+            TestInfos = new List<TestMethodInfo>();
+            foreach (MethodInfo testMethod in testMethods)
+            {
+                if (!testMethod.CustomAttributes.Any(val => val.AttributeType == typeof(TestAttribute)))
+                {
+                    return;
+                }
+                TestInfos.Add(new TestMethodInfo(testMethod));
+            }
+        }
+        public string TestClassName { get; set; }
+        public string[] IgnoreValues { get; set; }
+        public List<TestMethodInfo> TestInfos { get; set; }
+    }
+}
+
---
+++ TestStats/TestMethodInfo.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStats
+{
+    public class TestMethodInfo
+    {
+        public TestMethodInfo(MethodInfo methodInfo)
+        {
+            if (!methodInfo.CustomAttributes.Any(val => val.AttributeType == typeof(TestAttribute)))
+            {
+                return;
+            }
+            IEnumerable<CustomAttributeData> attrDataList = methodInfo.CustomAttributes;
+            IEnumerable<CustomAttributeData> authorAttrData = attrDataList.Where(val => val.AttributeType == typeof(AuthorAttribute)).Distinct();
+            IEnumerable<CustomAttributeData> ignoreAttrData = attrDataList.Where(val => val.AttributeType == typeof(IgnoreAttribute)).Distinct();
+            TestMethodName = methodInfo.Name;
+            Author = authorAttrData.Any()
+                        ? authorAttrData.ToList().FirstOrDefault().ConstructorArguments.FirstOrDefault().Value.ToString()
+                        : "NoName"; ;
+            IgnoreValues = ignoreAttrData.Any()
+                        ? ignoreAttrData.ToList().FirstOrDefault().ConstructorArguments.Where(val => val.ArgumentType == typeof(String))
+                            .FirstOrDefault().Value.ToString().Split(',')
+                            .Select(val => val.Trim()).ToArray()
+                        : default;
+            TestCasesCount = attrDataList.Any(val => val.AttributeType == typeof(TestCaseAttribute))
+                ? attrDataList.Count(val => val.AttributeType == typeof(TestCaseAttribute))
+                : 1;
+        }
+        public string TestMethodName { get; set; }
+        public string Author { get; set; }
+        public int TestCasesCount { get; set; }
+        public string[] IgnoreValues { get; set; }
+    }
+}
+
